8687afa0417bafb8eb0b67f193c84e34c3cc16a2
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-13 10:15:31 +0100

    aggiornamento home

--- react-router/src/pages/Home.jsx
+++ react-router/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <p>Benvenuto nel nostro blog!</p>
+      <h1>Ristorante Tana Toscana</h1>
+      <p>
+        Benvenuti nel cuore della Toscana! 🍷🍽️ Nel nostro ristorante celebriamo
+        la tradizione culinaria toscana con ingredienti genuini, ricette
+        autentiche e un’atmosfera calda e accogliente. Dalla bistecca alla
+        fiorentina ai pici fatti a mano, ogni piatto racconta la storia della
+        nostra terra, accompagnato dai migliori vini locali. Vieni a scoprire i
+        sapori della Toscana, dove ogni pasto è un’esperienza da ricordare!
+      </p>
     </div>
   );
 };
