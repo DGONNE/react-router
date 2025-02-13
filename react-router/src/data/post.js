bf615918bb821e6df5c18b53cd123a13b9397d0e
Davide Gonnelli <davide.gonnelli1@gmail.com>
2025-02-13 10:21:59 +0100

    creazione dati per post.jsx

---
+++ react-router/src/data/post.js
@@ -0,0 +1,37 @@
+export const dishes = [
+  {
+    id: 1,
+    name: "Bistecca alla Fiorentina",
+    description:
+      "Taglio di carne spessa, cotta alla brace e servita al sangue, un simbolo della cucina toscana.",
+    price: "35€",
+  },
+  {
+    id: 2,
+    name: "Pappa al Pomodoro",
+    description:
+      "Zuppa rustica a base di pane raffermo, pomodoro, basilico e olio extravergine d’oliva.",
+    price: "10€",
+  },
+  {
+    id: 3,
+    name: "Pici Cacio e Pepe",
+    description:
+      "Pasta fresca toscana condita con pecorino toscano DOP e pepe nero.",
+    price: "14€",
+  },
+  {
+    id: 4,
+    name: "Ribollita",
+    description:
+      "Zuppa di verdure e pane, ricca di sapori autentici e perfetta nelle fredde giornate invernali.",
+    price: "12€",
+  },
+  {
+    id: 5,
+    name: "Tagliere Toscano",
+    description:
+      "Selezione di salumi e formaggi toscani, accompagnati da miele e marmellate artigianali.",
+    price: "18€",
+  },
+];
